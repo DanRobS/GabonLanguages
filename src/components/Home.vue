f8a516dd5d7b803453803224528b05294bb6e5bf
DanRobS <roberttsoumbou@gmail.com>
2021-02-06 20:29:57 +0100

    Home component update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="home-div" class="container">
 
-    HOME
+    <section class="section is-medium">
+      <h1 class="title">Accueil</h1>
+      <h2 class="subtitle">
+        A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading.
+      </h2>
+    </section>
 
   </div>
 </template>
